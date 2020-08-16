81316bbc80a337eb8d9a69ce21e2cd5e165b5542
Maximiliano Yacobucci <maxyacobucci@gmail.com>
2020-08-16 12:40:56 -0300

    Expand employee expected model

--- employeers-server/webapp/apis/employeers.py
+++ employeers-server/webapp/apis/employeers.py
@@ -5,15 +5,20 @@ from ..lib.company_resources import get_employees_from_company_resources, get_em
 
 api = Namespace('employeers', description='Employeers')
 
-employeer = api.model('Employeer', {
-    'id': fields.String(required=True, description='The employeer identifier'),
+employee_model = api.model('Employeer', {
+    'id': fields.Integer(required=True, description='The employee identifier'),
+    'first': fields.String(description='The employee first name'),
+    'last': fields.String(description='The employee last name'),
+    'manager': fields.Integer(description='The employee manager'),
+    'department': fields.Integer(description='The employee department'),
+    'office': fields.Integer(description='The employee office'),
 })
 
 
 @api.route('')
 class Employeers(Resource):
     @api.doc('list_employeers', params=DOC_PAGINATION_ARGUMENTS)
-    @api.marshal_list_with(employeer)
+    @api.marshal_list_with(employee_model)
     @use_kwargs(PAGINATION_ARGUMENTS, location="query")
     def get(self, limit, offset):
         params = {"limit": limit, "offset": offset}
@@ -25,7 +30,7 @@ class Employeers(Resource):
 @api.response(404, 'Employeer not found')
 class Employeer(Resource):
     @api.doc('get_employeer')
-    @api.marshal_with(employeer)
+    @api.marshal_with(employee_model)
     def get(self, identifier):
         validate_id(identifier)
 
